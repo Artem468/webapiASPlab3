5fa733cd217653a363754e1bf9453aadd4e43c6d
Artem <artem.belykh.ru@gmail.com>
2024-12-05 16:11:49 +0200

    add jwt

--- webapiASP/Models/DataContext.cs
+++ webapiASP/Models/DataContext.cs
@@ -7,4 +7,5 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions options) : base(options){}
     public DbSet<Prices> Prices => Set<Prices>();
     public DbSet<RegistrationProduct> RegistrationProduct => Set<RegistrationProduct>();
+    public DbSet<Persons> Users => Set<Persons>();
 }
--- webapiASP/obj/Debug/net8.0/webapiASP.AssemblyInfo.cs
+++ webapiASP/obj/Debug/net8.0/webapiASP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapiASP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c923de1e808f56973c73fde4344cb949a1f63eb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapiASP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapiASP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
